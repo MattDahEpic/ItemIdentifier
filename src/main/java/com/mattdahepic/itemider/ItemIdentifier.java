41d58a657c60dff84b81fbd8ba61a8011f6a0897
Matt <mattdahepic@gmail.com>
2015-02-04 09:09:56 -0700

    ready for distribution, sad that no one besides me tested it...

--- src/main/java/com/mattdahepic/itemider/ItemIdentifier.java
+++ src/main/java/com/mattdahepic/itemider/ItemIdentifier.java
@@ -18,7 +18,7 @@ public class ItemIdentifier {
 
     public static final String MODID = "ItemIdentifier";
     public static final String NAME = "Item Identifier";
-    public static final String VERSION = "0.5";
+    public static final String VERSION = "1.0";
 
     @SidedProxy(clientSide = "com.mattdahepic.itemider.ClientProxy",serverSide = "com.mattdahepic.itemider.CommonProxy")
     public static CommonProxy proxy;
